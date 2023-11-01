8e9a32fa87f6e4a24aea6aaa2930cd209d3b8447
Janelle Cai <janellexcai@gmail.com>
2023-10-31 17:47:59 -0700

    implemented the AGB function, completed docstrings

--- forest_carbon/single_tree_estimation.py
+++ forest_carbon/single_tree_estimation.py
@@ -1,43 +1,21 @@
 """
-Implementing the allometric model in the paper "Improved allometric models to estimate
-the aboveground biomass of tropical trees" by Chave et. al.
+Implementing the allometric model to estimate the aboveground biomass of a tree based
+on the paper "Improved allometric models to estimate the aboveground biomass of tropical
+trees" by Chave et. al.
 """
 
-import math
 
-
-def find_sigma(N, p, e):
-    """
-    Returns the RSE for a model given the following arguments.
-    N: sample size
-    p: number of parameters
-    e: array containing the error for each sample
-    """
-    square_sum = 0
-    for error in e:
-        square_sum += error**2
-    return math.sqrt(1 / (N - p) * square_sum)
-
-
-def create_AGB_function(alpha, beta, sigma, sigmap):
+def create_AGB_function(coef, exp):
     """
     Returns a function that takes in the parameters rho, d, and h and outputs the
     estimated AGB.
 
     Arguments:
-    alpha, beta: model coefficients derived from least squares regression
-    sigma: residual standard error (RSE)
-    sigmap: TODO
+    coef, exp: the parameters that were fitted to the exponential AGB model.
     """
 
     def AGB_function(rho, d, h):
-        return math.exp(
-            sigma**2 / 2
-            + (beta**2) * (sigmap**2) / 2
-            + alpha
-            + beta * math.log(rho * d**2)
-            + beta * math.log(h)
-        )
+        return coef * (rho * d**2 * h) ** exp
 
     return AGB_function
 
@@ -45,5 +23,10 @@ def create_AGB_function(alpha, beta, sigma, sigmap):
 def apply_AGB_model(agb, rho, d, h):
     """
     Returns the estimate for the AGB given a model to apply (agb), rho, d, and h.
+
+    Arguments:
+    rho: wood specific gravity (g/cm^3)
+    d: trunk diameter (cm)
+    h: total tree height (m)
     """
     return agb(rho, d, h)
