39bdd6b7054b7504628643c740a32b101424fd43
Charles Dawson <charles.dwsn@gmail.com>
2023-11-10 10:18:38 -0500

    Add unit tests for tree preprocessing

--- forest_carbon/tree_preprocessing.py
+++ forest_carbon/tree_preprocessing.py
@@ -28,16 +28,55 @@ def create_common_name_dictionary(csv_file_path):
     return common_name_data
 
 
-if __name__ == "__main__":
-    # Example usage:
-    csv_file = "forest_carbon/data/tree_species_info.csv"
-    resulting_dictionary = create_common_name_dictionary(csv_file)
+def preprocess_tree_entry(tree: dict, database: dict) -> dict:
+    """Preprocess a tree entry to add information on the taxa, group, etc.
 
-    # Print the resulting dictionary
-    for common_name, data in resulting_dictionary.items():
-        print(f"Common Name: {common_name}")
-        print(f"Taxa: {data['taxa']}")
-        print(f"Group: {data['group']}")
-        print(f"FIA Species: {data['fia_species_code']}")
-        print(f"Wood Specific Gravity: {data['spg']}")
-        print("-" * 30)
+    Args:
+        tree (dict): A dictionary containing information on a single tree. Should
+            include a "species" tag with a label for the species.
+        database (dict): A dictionary containing information on the species, generated
+            by the `create_common_name_dictionary` function.
+
+    Returns:
+        dict: The input dictionary with additional information added on taxa, group,
+            and specific gravity.
+
+    Raises:
+        KeyError: If the species label is not found in the common name dictionary.
+    """
+    # Extract the species label
+    species = tree["species"]
+
+    # Extract the data from the database
+    taxa = database[species]["taxa"]
+    group = database[species]["group"]
+    spg = database[species]["spg"]
+    fia_species_code = database[species]["fia_species_code"]
+
+    # Add the data to the tree dictionary
+    tree["taxa"] = taxa
+    tree["group"] = group
+    tree["spg"] = spg
+    tree["fia_species_code"] = fia_species_code
+
+    return tree
+
+
+def preprocess_tree_entries(trees: list, database: dict) -> list:
+    """Preprocess a list of tree entries.
+
+    Args:
+        trees (list): A list of dictionaries containing information on trees. Each
+            dictionary should include a "species" tag with a label for the species.
+        database (dict): A dictionary containing information on the species, generated
+            by the `create_common_name_dictionary` function.
+
+    Returns:
+        list: The input list with additional information added on taxa, group,
+            and specific gravity.
+
+    Raises:
+        KeyError: If the species label of any tree is not found in the common name
+            dictionary.
+    """
+    return [preprocess_tree_entry(tree, database) for tree in trees]
